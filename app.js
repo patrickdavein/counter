a2b4246df997d919aa6440a7cb638137a77eb56d
Patrick Dave Intervalo <72492512+patrickdavein@users.noreply.github.com>
2020-10-16 19:26:09 +0800

    script commit

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-let count = 0;
-// select value & buttons
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+let count = 0
 
-btns.forEach(function (btn) {
+const value = document.querySelector("#value")
+const btns = document.querySelectorAll(".btn")
+
+btns.forEach( (btn) => {
   btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
+    const styles = e.currentTarget.classList
     if (styles.contains("decrease")) {
-      count--;
+      count--
     } else if (styles.contains("increase")) {
-      count++;
+      count++
     } else {
-      count = 0;
+      count = 0
     }
 
     if (count > 0) {
-      value.style.color = "green";
+      value.style.color = "green"
     }
     if (count < 0) {
-      value.style.color = "red";
+      value.style.color = "red"
     }
     if (count === 0) {
-      value.style.color = "#222";
+      value.style.color = "#222"
     }
-    value.textContent = count;
-  });
-});
+    value.textContent = count
+  })
+})
